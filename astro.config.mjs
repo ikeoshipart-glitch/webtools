184d329065e4b1b215923f8ba8ed80187a3bfac5
ikeoshipart-glitch <iketomi.oshigoto.part@gmail.com>
2026-08-07 00:03:41 +0900

    公開URLにSEO設定を更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://webtools.pages.dev',
+  site: 'https://webtools-6bz.pages.dev',
   integrations: [sitemap()],
 });
